a9fc89a8e790c7c859012dac831bccef638eac9f
t-k-satoh <syakuyanaoto@gmail.com>
2020-11-15 20:44:52 +0900

    fix: 🐛 bug fix

--- src/components/pages/products/component.tsx
+++ src/components/pages/products/component.tsx
@@ -5,22 +5,5 @@ import { Wrapper } from '../../utility/wrapper';
 import * as Styles from './styles';
 
 export const Products: React.FC<State> = ({ products }) => {
-    const columns: ComponentProps<typeof Table>['columns'] = React.useMemo(() => {
-        if (products.length === 0) {
-            return [];
-        }
-
-        const keys = Object.keys(products[0]);
-
-        return keys.map((key) => {
-            return {
-                title: pascalcase(key),
-                key,
-            };
-        });
-    }, products);
-
-    console.log(columns, products);
-
     return <Wrapper isLoading={false} canRender={true}></Wrapper>;
 };
